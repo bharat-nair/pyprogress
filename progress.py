cf1071081d03910711ed4a32568fea095d5f937c
Bharat Nair <bharatsnair@gmail.com>
2019-03-31 23:07:59 +0530

    change print to stdout and stderr

--- progress.py
+++ progress.py
@@ -1,22 +1,27 @@
 import time
+import sys
 from urllib.request import urlretrieve
 
 def progress(downloaded, block_size, total_size):
     global download_size
     download_size = total_size
     completed = round(downloaded / (total_size//block_size) * 100)
-    print("\r|" + "█" * completed + " " * (100-completed) + "|{}%".format(completed),end="")
-    
+    sys.stdout.write("\r|" + "█" * completed + " " * (100-completed) + "|{}%".format(completed),end="")
+
 def download_bar(url,filename):
     start = time.time()
-    urlretrieve(url,filename,progress)
+    try:
+        urlretrieve(url,filename,progress)
+    except:
+        sys.stderr.write("An unknown error occured.")
+        exit(0)
     end = time.time()
     print("\n",end="")
     time_taken = end - start
     download_speed = round(download_size/(time_taken*1024),2)
     if download_speed > 1024:
-        print("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed/1024) + " MB/s)" + "\n",end="")
+        sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed/1024) + " MB/s)" + "\n")
     if download_speed < 1024:
-        print("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed) + " kB/s)" + "\n",end="")
+        sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed) + " kB/s)" + "\n")
     if download_speed < 1:
-        print("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed*1024) + " B/s)" + "\n",end="")
+        sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed*1024) + " B/s)" + "\n")
