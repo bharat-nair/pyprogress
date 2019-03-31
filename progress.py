ca63a49216845f294c86537ed477fb84b39fd2b3
Bharat Nair <bharatsnair@gmail.com>
2019-03-31 19:00:17 +0530

    add MB/s and B/s

--- progress.py
+++ progress.py
@@ -1,32 +1,22 @@
 import time
-import sys
 from urllib.request import urlretrieve
 
 def progress(downloaded, block_size, total_size):
     global download_size
     download_size = total_size
     completed = round(downloaded / (total_size//block_size) * 100)
-    sys.stdout.write("\r|" + "█" * completed + " " * (100-completed) + "|{}%".format(completed))
+    print("\r|" + "█" * completed + " " * (100-completed) + "|{}%".format(completed),end="")
     
-def simple_bar(dur=10,bar="█"):
-
-    for i in range(dur+1):
-        perc = str(round((i/dur)*100))
-        sys.stdout.write("\r|" + bar * i + " " * (dur-i) + "|{}%".format(perc))
-        sys.stdout.flush()
-        time.sleep(0.5)
-    sys.stdout.write("\n")
-
 def download_bar(url,filename):
     start = time.time()
     urlretrieve(url,filename,progress)
     end = time.time()
-    sys.stdout.write("\n")
+    print("\n",end="")
     time_taken = end - start
-    download_speed = round(download_size/(time_taken*1000),2)
-    sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed) + " kB/s)" + "\n")
-
-
-download_bar("ftp://speedtest:speedtest@ftp.otenet.gr/test1Mb.db","test1.db")
-
-# simple_bar(50,'.')
+    download_speed = round(download_size/(time_taken*1024),2)
+    if download_speed > 1024:
+        print("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed/1024) + " MB/s)" + "\n",end="")
+    if download_speed < 1024:
+        print("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed) + " kB/s)" + "\n",end="")
+    if download_speed < 1:
+        print("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed*1024) + " B/s)" + "\n",end="")
