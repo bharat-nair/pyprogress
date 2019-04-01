6daf878c469ce130745198dd6fc8232efe4e574d
ubuntu <bharatsnair@gmail.com>
2019-04-01 13:44:35 +0530

    added condition for speed = 1024

--- progress.py
+++ progress.py
@@ -1,27 +1,34 @@
 import time
 import sys
 from urllib.request import urlretrieve
+from urllib.error import URLError
 
 def progress(downloaded, block_size, total_size):
     global download_size
     download_size = total_size
-    completed = round(downloaded / (total_size//block_size) * 100)
+    completed = int(downloaded / (total_size//block_size) * 100)
     sys.stdout.write("\r|" + "█" * completed + " " * (100-completed) + "|{}%".format(completed))
 
 def download_bar(url,filename):
     start = time.time()
     try:
         urlretrieve(url,filename,progress)
-    except:
-        sys.stderr.write("An unknown error occured.")
+    except URLError:
+        sys.stderr.write("The URL is invalid.\n")
+        exit(0)
+    except Exception as e:
+        sys.stderr.write("An error occured\n")
+        sys.stderr.write(type(e),e)
         exit(0)
     end = time.time()
     print("\n",end="")
     time_taken = end - start
     download_speed = round(download_size/(time_taken*1024),2)
-    if download_speed > 1024:
+    if download_speed >= 1024:
         sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed/1024) + " MB/s)" + "\n")
     if download_speed < 1024:
         sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed) + " kB/s)" + "\n")
     if download_speed < 1:
         sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed*1024) + " B/s)" + "\n")
+
+download_bar("https://sample-videos.com/text/Sample-text-file-1000kb.txt","test.txt")
